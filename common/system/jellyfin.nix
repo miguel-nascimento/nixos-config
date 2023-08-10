ee4fbe3739686294e7b6193bf247eb1524f024a7
Miguel Nascimento <miguelgomes13@live.com>
2023-08-09 23:07:56 -0300

    oops

--- common/system/jellyfin.nix
+++ common/system/jellyfin.nix
@@ -1,6 +1,6 @@
 let 
   # https://github.com/jellyfin/jellyfin-plugin-anidb
-  anidbMetadata = {
+  anidbMetadata = pkgs.stdenv.mkDerivation {
     name = "jellyfin-plugin-anidb";
     src = pkgs.fetchurl {
       url = "https://repo.jellyfin.org/releases/plugin/anidb/anidb_7.0.0.0.zip";
@@ -12,7 +12,7 @@ let
     unpackCmd = "unzip -d $out $curSrc";
   };
   # https://github.com/vosmiic/jellyfin-ani-sync
-  aniSyncPlugin = {
+  aniSyncPlugin = pkgs.stdenv.mkDerivation {
     name = "jellyfin-ani-sync";
     src = pkgs.fetchurl {
       url = "https://github.com/vosmiic/jellyfin-ani-sync/releases/download/v2.9/10.8.10.-.ani-sync_2.9.0.0.zip";
@@ -27,7 +27,7 @@ let
     name = "custom-jellyfin";
     src = pkgs.jellyfin;
     buildInputs = [ pkgs.unzip ];
-    fixupPhase = "cp -R ${anidbMetadata}/ $out/plugins/${anidbMetadata}/; cp -R ${aniSyncPlugin}/ $out/plugins/${aniSyncPlugin}/;";
+    fixupPhase = "cp -R ${anidbMetadata}/* $out/plugins/${anidbMetadata}/; cp -R ${aniSyncPlugin}/* $out/plugins/${aniSyncPlugin}/;";
   };
 in
 {
