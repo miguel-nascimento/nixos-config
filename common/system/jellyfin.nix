f294584193000c8dd669c49e73b620f655474a00
Miguel Nascimento <miguelgomes13@live.com>
2023-08-09 22:56:39 -0300

    wip jellyfin w/ plugins

--- common/system/jellyfin.nix
+++ common/system/jellyfin.nix
@@ -1,6 +1,38 @@
-_:
+let 
+  # https://github.com/jellyfin/jellyfin-plugin-anidb
+  anidbMetadata = {
+    name = "jellyfin-plugin-anidb";
+    src = pkgs.fetchurl {
+      url = "https://repo.jellyfin.org/releases/plugin/anidb/anidb_7.0.0.0.zip";
+      sha256 = "sha256-anidb-GRAB-FROM-ERROR";
+    };
+    nativeBuildInputs = [ pkgs.unzip ];
+    dontInstall = true;
+    sourceRoot = ".";
+    unpackCmd = "unzip -d $out $curSrc";
+  };
+  # https://github.com/vosmiic/jellyfin-ani-sync
+  aniSyncPlugin = {
+    name = "jellyfin-ani-sync";
+    src = pkgs.fetchurl {
+      url = "https://github.com/vosmiic/jellyfin-ani-sync/releases/download/v2.9/10.8.10.-.ani-sync_2.9.0.0.zip";
+      sha256 = "sha256-ani-sync-GRAB-FROM-ERROR";
+    };
+    nativeBuildInputs = [ pkgs.unzip ];
+    dontInstall = true;
+    sourceRoot = ".";
+    unpackCmd = "unzip -d $out $curSrc";
+  };
+  jellyfinWithPlugins = {
+    name = "custom-jellyfin";
+    src = pkgs.jellyfin;
+    buildInputs = [ pkgs.unzip ];
+    fixupPhase = "cp -R ${anidbMetadata}/ $out/plugins/${anidbMetadata}/; cp -R ${aniSyncPlugin}/ $out/plugins/${aniSyncPlugin}/;";
+  };
+in
 {
   services.jellyfin = {
+    package = jellyfinWithPlugins;
     enable = true;
     openFirewall = true;
   };
