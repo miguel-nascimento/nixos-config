30f5c5123961dccae34ee2d8943bca5eba87b128
Miguel Nascimento <miguelgomes13@live.com>
2024-02-17 01:34:23 -0300

    fix(nvim): setup comment.nvim

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -19,5 +19,9 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+    config = function()
+      require('todo-comments').setup({})
+    end
   }
 }
