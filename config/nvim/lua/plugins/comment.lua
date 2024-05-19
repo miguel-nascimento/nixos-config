7a89a964f7780d6700e5a1384ce120a2dfb86c2d
Miguel Nascimento <miguelgomes13@live.com>
2024-05-19 19:22:37 -0300

    feat: neovim 0.10
    geez the inlay hints looks sooo yummy

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,22 +1,5 @@
 return {
   {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    lazy = true,
-    opts = {
-      enable_autocmd = false,
-    },
-  },
-  -- using comment.nvim only for block comments -- planning to switch to mini-comment if I not use it on daily usage
-  {
-    'numToStr/Comment.nvim',
-    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
-    config = function()
-      require('Comment').setup {
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-      }
-    end,
-  },
-  {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
