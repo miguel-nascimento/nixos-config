7c94400e05e034386b160c99e67a64bc796190e3
Miguel Nascimento <miguelgomes13@live.com>
2024-08-11 06:34:15 +0000

    chore: tmux-ressurect & tmux-continuum

--- common/home-manager/programs/tmux.nix
+++ common/home-manager/programs/tmux.nix
@@ -30,6 +30,17 @@
         '';
       }
       { plugin = tmuxPlugins.vim-tmux-navigator; }
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+            set -g @continuum-restore 'on'
+            set -g @continuum-save-interval '60' # minutes
+        '';
+      }
     ];
     extraConfig = ''
       set -g default-terminal "screen-256color"
