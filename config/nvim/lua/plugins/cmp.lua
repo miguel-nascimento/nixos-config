41742d66716644049bccac26c6794b4efa70bb1d
Miguel Nascimento <miguelgomes13@live.com>
2024-12-19 04:28:01 +0000

    feat(nvim): move from cmp-nvim to blink.cmp

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -1,95 +1,30 @@
----@diagnostic disable: missing-fields
--- Kindly stolen from dmulroy nvim kickstart.nix <3
 return {
-  {
-    'hrsh7th/nvim-cmp',
-    event = { 'BufReadPost', 'BufNewFile' },
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'rafamadriz/friendly-snippets',
-      'onsails/lspkind.nvim',
-      'windwp/nvim-ts-autotag',
-      'windwp/nvim-autopairs',
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = 'rafamadriz/friendly-snippets',
+  version = 'v0.*',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- see the "default configuration" section below for full documentation on how to define
+    -- your own keymap.
+    keymap = { preset = 'enter' },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono',
     },
-    config = function()
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      local lspkind = require 'lspkind'
-
-      require('nvim-autopairs').setup()
-
-      -- Integrate nvim-autopairs with cmp
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-
-      -- Load snippets
-      require('luasnip.loaders.from_vscode').lazy_load()
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert {
-          ['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
-          ['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<C-u>'] = cmp.mapping.scroll_docs(4), -- scroll up preview
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4), -- scroll down preview
-          ['<C-Space>'] = cmp.mapping.complete {}, -- show completion suggestions
-          ['<C-c>'] = cmp.mapping.abort(), -- close completion window
-          ['<CR>'] = cmp.mapping.confirm { select = true }, -- select suggestion
-        },
-        -- sources for autocompletion
-        sources = cmp.config.sources {
-          { name = 'nvim_lsp' }, -- lsp
-          { name = 'buffer', max_item_count = 5 }, -- text within current buffer
-          { name = 'supermaven' }, -- Copilot suggestions
-          { name = 'path', max_item_count = 3 }, -- file system paths
-          { name = 'luasnip', max_item_count = 3 }, -- snippets
-        },
-        -- Enable pictogram icons for lsp/autocompletion
-        formatting = {
-          expandable_indicator = true,
-          format = lspkind.cmp_format {
-            mode = 'symbol_text',
-            maxwidth = 80,
-            ellipsis_char = '...',
-            symbol_map = {
-              Supermaven = '',
-            },
-          },
-        },
-        experimental = {
-          ghost_text = true,
-        },
-      }
-    end,
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+    signature = { enabled = true },
   },
+  -- -- allows extending the providers array elsewhere in your config
+  -- -- without having to redefine it
+  opts_extend = { 'sources.default' },
 }
