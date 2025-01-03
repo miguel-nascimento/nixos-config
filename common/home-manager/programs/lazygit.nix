827f0066a2c018fc56337625be32ce86c2cc79ad
Miguel Nascimento <miguelgomes13@live.com>
2025-01-03 17:53:01 -0300

    chore: lazygit edit in nvim-remote

--- common/home-manager/programs/lazygit.nix
+++ common/home-manager/programs/lazygit.nix
@@ -2,6 +2,7 @@ _: {
   programs.lazygit = {
     enable = true;
     settings = {
+      os.editPreset = "nvim-remote";
       gui.showIcons = true;
       git.paging = {
         colorArgs = "always";
