603accc8b6edfd6d6e02ea57aec776952b7a74dd
Miguel Nascimento <miguelgomes13@live.com>
2025-09-15 15:56:41 -0300

    jesus christ pls help me

--- common/home-manager/languages/rust.nix
+++ common/home-manager/languages/rust.nix
@@ -3,9 +3,14 @@
   home.packages = with pkgs; [
     rustup
     libiconv
+    zlib
+    pkg-config
   ];
 
   home.sessionVariables = {
-    LIBRARY_PATH = "${pkgs.libiconv}/lib";
+    LIBRARY_PATH = "${pkgs.libiconv}/lib:${pkgs.zlib}/lib";
+    PKG_CONFIG_PATH = "${pkgs.libiconv}/lib/pkgconfig:${pkgs.zlib}/lib/pkgconfig";
+    # Add rustflags for rust-lld to find libraries
+    RUSTFLAGS = "-L native=${pkgs.libiconv}/lib -L native=${pkgs.zlib}/lib -liconv -lz";
   };
 }
